48ad3b8250bec8ef3f4a2054df0b649259b35f05
NogueraMateo <mateo.edu.co@gmail.com>
2025-05-29 23:31:50 -0500

    enhancemente: Adapt helpers to boto3.client

--- core/aws/helpers.py
+++ core/aws/helpers.py
@@ -4,11 +4,30 @@ from core.aws.setup import s3
 import os
 
 def upload_file_to_s3(file, key):
-    s3.Bucket(os.environ.get('AWS_STORAGE_BUCKET_NAME')).put_object(Key=key, Body=file)
-    url = f"https://{os.environ.get('AWS_STORAGE_BUCKET_NAME')}.s3.amazonaws.com/{key}"
+    """
+    Uploads a file to Amazon S3
+    :param file: File to upload
+    :param key: Path where the file is going to be stored in the cloud. Ej. 'folder/nested/file.ext'
+    :return: The url of the file.
+    """
+    bucket_name = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=key,
+        Body=file
+    )
+    url = f"https://{bucket_name}.s3.amazonaws.com/{key}"
     return url
 
 def create_presigned_url(key, expiration=3600):
+    """
+    Create a presigned url so the frontend can render private files. The
+    generated url has an expiration time. Default to 1 hour.
+    :param key: Path where the file is stored in the cloud. Ej. 'folder/nested/file.ext'
+    :param expiration: Expiration time in seconds
+    :return: The presigned url of the file
+    """
     try:
         response = s3.generate_presigned_url(
             'get_object',
