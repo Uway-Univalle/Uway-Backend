94fce891f30ec712a4738cc2bf880277608f4b60
NogueraMateo <mateo.edu.co@gmail.com>
2025-07-13 16:11:17 -0500

    enhancement: Use uvicorn instead of daphne for development

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-daphne -b 0.0.0.0 -p 8000 uway_backend.asgi:application
+uvicorn uway_backend.asgi:application --host 0.0.0.0 --port 8000 --reload
