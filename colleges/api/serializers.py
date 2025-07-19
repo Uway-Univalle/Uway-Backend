bcaa3d467ab868e08495141704afb497d2f2b893
NogueraMateo <mateo.edu.co@gmail.com>
2025-07-19 08:30:34 -0500

    enhancement: Custom serializer for documentation purposes

--- colleges/api/serializers.py
+++ colleges/api/serializers.py
@@ -7,5 +7,24 @@ class CollegeSerializer(serializers.ModelSerializer):
     class Meta:
         model = College
         fields = '__all__'
+        extra_kwargs = {
+            'logo': {'read_only': True},
+            'is_verified': {'read_only': True},
+        }
+
+class CollegeCreateSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    address = serializers.CharField(max_length=150)
+    email = serializers.EmailField(max_length=320, required=False, allow_blank=True)
+    colors = serializers.ListField(
+        child=serializers.CharField(max_length=7, allow_blank=True, required=False),
+        required=True,
+        allow_empty=False
+    )
+    logo_img = serializers.FileField(
+        required=True,
+        allow_empty_file=False,
+        allow_null=False
+    )
 
 
