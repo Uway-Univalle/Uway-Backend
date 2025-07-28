3ee38a2edbc4f2820a49d4c8601ae83d56d36502
Keyner14 <you@example.com>
2025-07-27 20:15:52 -0500

    Get-users-endpoints

--- routes/api/urls.py
+++ routes/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from routes.api.views import create_full_route_from_coordinates, DriverRoutesView
+from routes.api.views import create_full_route_from_coordinates, DriverRoutesView, get_passenger_routes
 
 routes_router = DefaultRouter()
 
 urlpatterns = [
     path('routes/', DriverRoutesView.as_view()),
-    path('routes/full-route/', create_full_route_from_coordinates)
+    path('routes/full-route/', create_full_route_from_coordinates),
+    path( 'routes/<route_id>/', get_passenger_routes),
 ]
